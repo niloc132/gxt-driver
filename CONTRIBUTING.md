9e506745982a35393584248564e1a678e3a1e6fd
branflake2267@gmail.com <branflake2267@gmail.com>
2013-11-20 13:56:06 -0800

    Menu depth fix.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,32 @@
 #Contributing
 Fork, modify and create a pull against the master. 
 
+
 ##Formatting
-Use tabs with 4 spaces ending at 120 characters per line. 
+* Use tabs with 4 spaces ending at 120 characters per line. 
+* No spaces in string concatenation. 
+
+
+##Testing
+
+###Maven integration testing
+Running the integration tests with maven.
+
+* Check the `BaseTest` and change the web driver to firefox.
+* Run `mvn clean install -Pintegration-test`
+
+###IDE integration tests
+Building and debugging integration tests
+
+* Run `mvn clean install -Pintegration-test -DskipTests`
+* Run `mvn jetty:run -Pintegration-test`
+* Right click on test and click debug. 
+
+####Assertions
+* Turn on assertions when using Eclipse. Add `-ea` to the integration test VM Arguments.
+
+###Browser
+Dial up the server and goto: `http://127.0.0.1:9080/app/index.html?menubar` to see the GXT example.
+
+* "menubar" is the scenario name listed at the top of the test. 
+* If the scenario is missing it will error with a dialog: "Could not parse key".
--- src/test/java/org/senchalabs/gwt/gwtdriver/gxt/models/MenuBarTest.java
+++ src/test/java/org/senchalabs/gwt/gwtdriver/gxt/models/MenuBarTest.java
@@ -45,13 +45,14 @@ public class MenuBarTest extends BaseTest {
 
 		assert editMenu != null;
 	}
+
 	@Test
 	public void testHover() throws Exception {
 		MenuBar menuBar = GwtWidget.find(MenuBar.class, driver).withItem("File").done();
 
 		Menu fileMenu = menuBar.click("File");
 
-		//TODO hover over something else first, looks like 3.0.1 bug?
+		// TODO hover over something else first, looks like 3.0.1 bug?
 		fileMenu.mouseOver("Open...");
 
 		try {
@@ -73,4 +74,50 @@ public class MenuBarTest extends BaseTest {
 		}
 		newMenu.click("Database");
 	}
+
+	@Test
+	public void testDepthFound() throws Exception {
+		// Given a menu bar
+		MenuBar menuBar = GwtWidget.find(MenuBar.class, driver).withItem("File").done();
+
+		// And menu bar has menu
+		Menu fileMenu = menuBar.click("File");
+
+		// When hover over two items (3.0.1 issue), it opens up two menus
+		fileMenu.mouseOver("Open");
+		fileMenu.mouseOver("New");
+
+		// Then a depth of 0 and 1 can be found
+		Menu menuFound1 = GwtWidget.find(Menu.class, driver).atDepth(1).done();
+		Menu menuFound0 = GwtWidget.find(Menu.class, driver).atDepth(0).done();
+		
+		// And those menus have contents of
+		Assert.assertTrue(menuFound1.getElement().getText().contains("Open"));
+		Assert.assertTrue(menuFound0.getElement().getText().contains("Project"));
+	}
+
+	@Test
+	public void testDepthNotFound() {
+		// Given a menu bar
+		MenuBar menuBar = GwtWidget.find(MenuBar.class, driver).withItem("File").done();
+
+		// And menu bar has menu
+		Menu fileMenu = menuBar.click("File");
+
+		// When hover over two items (3.0.1 issue) and then a item that doesn't have a submenu 
+		fileMenu.mouseOver("Open");
+		fileMenu.mouseOver("New");
+		fileMenu.mouseOver("Open");
+
+		// Then a depth of none can be found, and exception is thrown
+		try {
+			GwtWidget.find(Menu.class, driver).atDepth(1).done();
+			Assert.fail();
+		} catch (AssertionError e) {
+			// expected
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// expected
+		}
+	}
+
 }
